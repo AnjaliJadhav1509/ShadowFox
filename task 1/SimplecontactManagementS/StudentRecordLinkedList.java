c0996ae023957ab4f6987a5ae330b5dca8b78347
AnjaliJadhav1509 <jadhavanjali486@gmail.com>
2024-08-29 19:24:59 +0530

    Update StudentRecordLinkedList.java

--- task 1/SimplecontactManagementS/StudentRecordLinkedList.java
+++ task 1/SimplecontactManagementS/StudentRecordLinkedList.java
@@ -1,92 +1,62 @@
 package student;
+import java.util.Scanner;
 
-	import java.util.Scanner;
-
-	// Class
 	public class StudentRecordLinkedList {
 
-		// Main driver method
 		 public static void main(String[] args)
 		{
 
-			// Creating HumanResourceOffice Object.
 			StudentRecordManagement hr = new StudentRecordManagement();
-
 			Record record = new Record();
 			
 				record.setIdNumber(6862);
 			record.setContactNumber(911);
 			record.setName("Ankit");
 
-			// Calling add() record method to
-			// add static data/(Hard CodedData) to linked List
 			hr.add(record);
-
-			// Creating Scanner Object to read input
 			Scanner input = new Scanner(System.in);
 
-			// Creating option integer variable
 			int option = 0;
-
-			// Do - While loop
 			do {
 				menu();
 				option = input.nextInt();
-
-				// Switch case
 				switch (option) {
 
-				// Case 1
 				case 1:
 
-					// Display message
 					System.out.print(
 						"What is the Student id Number ? ");
 
 					int idNumber = input.nextInt();
 
-					// Display message
 					System.out.print(
 						"What is the Student contact Number ? ");
 
 					int contactNumber = input.nextInt();
 					input.nextLine();
 
-					// Display message
 					System.out.print(
 						"What is the Student Name ? ");
 
 					String name = input.nextLine();
-
-					// Create record object and pass constructor
-					// parameters.
 					record = new Record(name, idNumber,
 										contactNumber);
-					// Call add() record
+					
 					hr.add(record);
 					System.out.println(record.toString());
-
-					// Break statement used to terminate program
-					// from here only once it entered this case
 					break;
 
-				// Case 2
 				case 2:
 
-					// Display message
 					System.out.print(
 						"What is the Student id number ? ");
 					int rId = input.nextInt();
 
-					// Invoke remove/delete record
 					hr.delete(rId);
-
 					break;
 
-				// Case 3
 				case 3:
 
-					// Display message
 					System.out.print(
 						"What is the Student id number? ");
 
@@ -95,10 +65,8 @@ package student;
 
 					break;
 
-				// Case 4
 				case 4:
 
-					// Display message
 					System.out.print(
 						"What is the Student id ? ");
 					int bookId = input.nextInt();
@@ -107,44 +75,30 @@ package student;
 						System.out.println(
 							"Student id does not exist\n");
 					}
-
 					break;
 
-				// Case 5
 				case 5:
 					hr.display();
 					break;
 
-				// Case 6
 				case 9:
 
-					// Display message
 					System.out.println(
 						"\nThank you for using the program. Goodbye!\n");
 					System.exit(0);
-
 					break;
-
-				// Case 7: Default case
-				// If none above case executes
 				default:
-
-					// Print statement
+					
 					System.out.println("\nInvalid input\n");
 					break;
 				}
 			}
 
-			// Checking condition
 			while (option != 9);
 		}
 
-		// Method 2
-		// Menu - Static menu for displaying options
-		public static void menu()
 		{
 
-			// Printing statements displaying menu on console
 			System.out.println("MENU");
 			System.out.println("1: Add Student");
 			System.out.println("2: Delete Student");
